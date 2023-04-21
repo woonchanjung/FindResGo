b412a784e82ba579589f09be8f06b33a73e3574d
xkrzypandax <xkrzypandax@gmail.com>
2023-04-21 13:00:46 -0700

    Fixing Icons

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import './Logo.css';
 export default function Logo() {
   return (
     <div className="Logo">
-      <img src="dist/assets/LogowithName.svg" alt="logo" />
+      <img src="dist/LogowithName.svg" alt="logo" />
     </div>
   );
 }
